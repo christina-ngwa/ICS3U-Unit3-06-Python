e11bc5cadd05cb6dd95bf41369a6f647b821dac5
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-10-09 04:15:52 +0000

    1

---
+++ try_rdn_num.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Created by: Christina Ngwa
+# Created on: October 2019
+# this program checks if the user guess the number
+#    with a try statement
+
+
+import random
+
+
+def main():
+    # this function uses a try statement
+
+    # input
+    print("Guess what number I'm thinking of! (between 1 & 100)")
+    randomanswer = random.randint(1, 100)  # a number between 1 and 100
+    guess = input("Enter your guess: ")
+    print("")
+
+    # process
+    try:
+        guess_as_num = int(guess)
+        if guess == randomanswer:
+            # output
+            print("You are correct!")
+        else:
+            print("Sorry, the number I was thinking of was", randomanswer, ".")
+    except Exception:
+        print("Wrong input. Try again.")
+    finally:
+        print("Thanks for playing.")
+
+
+if __name__ == "__main__":
+    main()
